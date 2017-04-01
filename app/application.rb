b6b755ec5256da8f99dbcc869aabb514b14f2925
David Ordovskiy <david.ordovsky@gmail.com>
2017-04-01 10:54:50 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -5,9 +5,13 @@ class Application
     time = Time.now.strftime("%H").to_i
 
     if time < 12 
-      resp.write "Good Morning!"
+      resp.write "Good Morning!\n"
+      resp.write "The time is #{Time.now.hour}:#{Time.now.min}.\n"
+      resp.write "How about doing something productive?"
     else
       resp.write "Good Afternoon!"
+      resp.write "The time is #{Time.now.hour}:#{Time.now.min}.\n"
+      resp.write "Relax, get a beer, feel good about yourself."
     end
 
     resp.finish
