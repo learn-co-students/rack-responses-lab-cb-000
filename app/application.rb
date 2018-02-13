d907fdf9b53bf976b0d1c44bbafac02b62b36003
petitlapin86 <skinnerpa86@yahoo.com>
2018-02-13 15:05:49 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+
+class Application #creating a greeter that responds according to time of day
+
+  def call(env)
+    resp = Rack::Response.new
+    now = Time.now
+    if now.hour >=12
+      resp.write "Good Afternoon"
+    else
+      resp.write "Good Morning"
+    end
+
+    resp.finish
+  end
+
+end
